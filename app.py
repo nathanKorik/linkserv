571b1beb354830946596728d2e06d040a71c8f9b
unknown <nathankorik@gmail.com>
2026-05-20 22:32:38 -0300

    fix: cors manual via after_request e bind no procfile

--- app.py
+++ app.py
@@ -5,7 +5,12 @@ import mysql.connector
 import os
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}}, methods=["GET", "POST", "OPTIONS"])
+@app.after_request
+def add_cors_headers(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type,Authorization'
+    response.headers['Access-Control-Allow-Methods'] = 'GET,PUT,POST,DELETE,OPTIONS'
+    return response
 
 def get_db():
     return mysql.connector.connect(
